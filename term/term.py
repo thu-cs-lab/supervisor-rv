dbc0d5f268286fa7c64f118c3d4db042a41b7ae8
zhang <zz593141477@gmail.com>
2018-08-07 00:55:12 +0800

    use mips-mti-elf compiler in term.py

--- term/term.py
+++ term/term.py
@@ -23,10 +23,7 @@ try:
 except:
     pass
 
-if platform.system()=='Windows':
-    CCPREFIX = "mips-mti-elf-"
-else:
-    CCPREFIX = 'mipsel-linux-gnu-'
+CCPREFIX = "mips-mti-elf-"
 
 Reg_alias = ['zero', 'AT', 'v0', 'v1', 'a0', 'a1', 'a2', 'a3', 't0', 't1', 't2', 't3', 't4', 't5', 't6', 't7', 's0', 
                 's1', 's2', 's3', 's4', 's5', 's6', 's7', 't8', 't9/jp', 'k0', 'k1', 'gp', 'sp', 'fp/s8', 'ra']
