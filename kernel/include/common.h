5b277f3625bbfb52bbaa246e5f6ccf7107c4a911
Runji Wang <wangrunji0408@163.com>
2019-09-15 12:38:13 +0800

    simplify code and rename labels

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -1,12 +1,6 @@
 #ifndef _COMMON_H
 #define _COMMON_H
 
-#define SH_OP_R 0x0052              // char 'R'
-#define SH_OP_D 0x0044              // char 'D'
-#define SH_OP_A 0x0041              // char 'A'
-#define SH_OP_G 0x0047              // char 'G'
-#define SH_OP_T 0x0054              // char 'T'
-
 #ifdef RV32
 #define STORE sw
 #define LOAD lw
