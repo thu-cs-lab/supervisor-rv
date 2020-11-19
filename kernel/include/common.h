074ecef9246758fb06af5ec258512749bc8e2234
Runji Wang <wangrunji0408@163.com>
2020-11-19 19:46:26 +0800

    add timer interrupt

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -17,10 +17,17 @@
 #define SYS_exit 1
 #define SYS_putc 30
 
+#define MSTATUS_MIE (1 << 3)
 #define MSTATUS_MPP_MASK 0x1800
+
+#define MIE_MTIE (1 << 7)
 
 #define SATP_SV32 0x80000000
 #define SATP_SV39 0x8000000000000000
+
+#define CLINT 0x2000000
+#define CLINT_MTIME (CLINT + 0xBFF8)
+#define CLINT_MTIMECMP (CLINT + 0x4000)
 
 #ifdef ENABLE_PAGING
 #define USER_STACK_INIT 0x80000000
